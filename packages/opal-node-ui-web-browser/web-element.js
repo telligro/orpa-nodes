4cd5fbf59a725f5ddcaad689d397c7af42a7673a
Shiva Kumar Madhavan <shiva.madhavan@gmail.com>
2018-02-21 00:59:38 +0800

    refactor: remove verbose logs for web-page, web-element

--- packages/opal-node-ui-web-browser/web-element.js
+++ packages/opal-node-ui-web-browser/web-element.js
@@ -31,15 +31,15 @@ module.exports = function(RED) {
     const By = webdriver.By;
     const until = webdriver.until;
 
-    console.log('Registering Plugin Service');
+    // console.log('Registering Plugin Service');
     const FinderPluginSvc = require('@telligro/opal-page-object-finder').FinderPluginSvc;
     const dispatcher = require('@telligro/opal-page-object-finder').Dispatcher;
-    console.log('Registered Plugin Service: Page Object Finder');
+    // console.log('Registered Plugin Service: Page Object Finder');
     // const finderSvc = new FinderPluginSvc();
     // const dispatcher = new Dispatcher();
     dispatcher.registerObject('FinderPluginSvcElm', FinderPluginSvc);
     // console.log(finderSvc);
-    console.log(dispatcher);
+    // console.log(dispatcher);
     try {
         dispatcher.start(9010);
     } catch (ex) {
